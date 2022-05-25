afce679434492565fc2045c59ddd56425e491400
Tala-coder <muhammedtalic.it@gmail.com>
2022-05-25 02:49:57 +0200

    Update index.js

--- react-ispit-02/src/index.js
+++ react-ispit-02/src/index.js
@@ -5,13 +5,7 @@ import App from './components/App';
 import { initializeApp } from "firebase/app";  
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDx9mQro7sIyh9JiUUaSCWJYcLj3lYsPt4",
-  authDomain: "vjezbe-6.firebaseapp.com",
-  projectId: "vjezbe-6",
-  storageBucket: "vjezbe-6.appspot.com",
-  messagingSenderId: "1021086850860",
-  appId: "1:1021086850860:web:a67b05ee1a3d5e4e8c9689",
-  measurementId: "G-7L9BYCS619"
+  ...
 };
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
